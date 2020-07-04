7304c731a72aaf1228ce91a1196a7d9683accb5c
KrisiKostadinov <krisi.199898@gmail.com>
2020-07-04 15:57:28 +0300

    Initial template Express.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.use("/public", express.static(__dirname + '/public'));
+
+app.use('/', indexRouter);
+
+app.listen(2000, () => console.log('Server listening on port: ' + 2000));
